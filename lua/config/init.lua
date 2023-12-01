ceaf07f7757cbb253b1915c921d5122c60f61189
Jairus Patrick Vallon <jpatrickvallon@gmail.com>
2023-12-01 13:42:57 +0800

    Auto open nvim tree

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -40,3 +40,11 @@ local opts = {
 }
 
 require("lazy").setup('plugins', opts)
+
+local function open_nvim_tree()
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
